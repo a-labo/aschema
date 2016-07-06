3fafb324b1fc4c876a1598085f931a6de58a753c
Taka Okunishi <okunishitaka.com@gmail.com>
2016-07-06 21:31:34 +0900

    Version incremented to 1.0.3

--- sims/browser/index.js
+++ sims/browser/index.js
@@ -7,7 +7,7 @@
 var ASchema = require('./aschema');
 var create = require('./create');
 
-var lib = create.bind(lib);
+var lib = create.bind(undefined);
 
 Object.assign(lib, ASchema, {
   create: create,
