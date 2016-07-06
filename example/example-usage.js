44024986d2455857f2ae29005c01abce351ff4b8
Taka Okunishi <okunishitaka.com@gmail.com>
2016-07-06 21:14:16 +0900

    Initial commit

---
+++ example/example-usage.js
@@ -0,0 +1,3 @@
+'use strict'
+
+const aschema = require('aschema')
---
+++ test/aschema_test.js
@@ -0,0 +1,43 @@
+/**
+ * Test case for aschema.
+ * Runs with mocha.
+ */
+'use strict'
+
+const ASchema = require('../lib/aschema.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('aschema', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Aschema', () => co(function * () {
+    let productSchema = new ASchema(
+      require('../misc/mocks/mock-schemas/product')
+    )
+    {
+      let error = productSchema.validate({
+        id: 1234,
+        name: 'hoge'
+      })
+      assert.equal(error, null)
+    }
+    {
+      let error = productSchema.validate({
+        id: '1234',
+        name: { foo: 'bar' }
+      })
+      assert.ok(error)
+    }
+  }))
+})
+
+/* global describe, before, after, it */
