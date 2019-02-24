b67da07ff618dd9a660646962e0e7ea04a763623
okunishinishi <okunishinishi@gmail.com>
2019-02-24 19:06:16 +0900

    Version incremented to 3.0.1

--- shim/browser/create.js
+++ shim/browser/create.js
@@ -5,16 +5,21 @@
  */
 'use strict';
 
-var ASchema = require('./aschema');
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
+var _construct2 = _interopRequireDefault(require("@babel/runtime/helpers/construct"));
+
+var ASchema = require('./aschema');
 /** @lends create */
+
+
 function create() {
-  for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+  for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
     args[_key] = arguments[_key];
   }
 
-  return new (Function.prototype.bind.apply(ASchema, [null].concat(args)))();
+  return (0, _construct2.default)(ASchema, args);
 }
 
 module.exports = create;
-//# sourceMappingURL=data:application/json;base64,bnVsbA==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNyZWF0ZS5qcyJdLCJuYW1lcyI6WyJBU2NoZW1hIiwicmVxdWlyZSIsImNyZWF0ZSIsImFyZ3MiLCJtb2R1bGUiLCJleHBvcnRzIl0sIm1hcHBpbmdzIjoiQUFBQTs7Ozs7QUFLQTs7Ozs7O0FBRUEsSUFBTUEsT0FBTyxHQUFHQyxPQUFPLENBQUMsV0FBRCxDQUF2QjtBQUVBOzs7QUFDQSxTQUFTQyxNQUFULEdBQTBCO0FBQUEsb0NBQU5DLElBQU07QUFBTkEsSUFBQUEsSUFBTTtBQUFBOztBQUN4QixrQ0FBV0gsT0FBWCxFQUFzQkcsSUFBdEI7QUFDRDs7QUFFREMsTUFBTSxDQUFDQyxPQUFQLEdBQWlCSCxNQUFqQiIsInNvdXJjZVJvb3QiOiIuLi8uLi9saWIiLCJzb3VyY2VzQ29udGVudCI6WyIvKipcbiAqIENyZWF0ZSBhIHNjaGVtYSBpbnN0YW5jZVxuICogQGZ1bmN0aW9uIGNyZWF0ZVxuICogQHJldHVybnMge0FTY2hlbWF9IC0gU2NoZW1hIGluc3RuYWNlXG4gKi9cbid1c2Ugc3RyaWN0J1xuXG5jb25zdCBBU2NoZW1hID0gcmVxdWlyZSgnLi9hc2NoZW1hJylcblxuLyoqIEBsZW5kcyBjcmVhdGUgKi9cbmZ1bmN0aW9uIGNyZWF0ZSAoLi4uYXJncykge1xuICByZXR1cm4gbmV3IEFTY2hlbWEoLi4uYXJncylcbn1cblxubW9kdWxlLmV4cG9ydHMgPSBjcmVhdGVcbiJdfQ==
